78374a976bd516b2274372c675da3ed9aadb82b0
EvanVerdu <89754847+EvanVerdu@users.noreply.github.com>
2021-09-11 17:08:09 -0400

    Add files via upload

---
+++ verdu-cop3330-ex04/src/main/java/Exercise04/Exercise04.java
@@ -0,0 +1,32 @@
+package Exercise04;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Evan Verdu
+ */
+
+import java.util.Scanner;
+
+public class Exercise04 {
+
+    public static void main (String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a noun: ");
+        String noun = scanner.nextLine();
+
+        System.out.print("Enter a verb: ");
+        String verb = scanner.nextLine();
+
+        System.out.print("Enter an adjective: ");
+        String adjective = scanner.nextLine();
+
+        System.out.print("Enter an adverb: ");
+        String adverb = scanner.nextLine();
+
+        System.out.print("Do you " + verb + " your " + adjective + " " + noun + " " + adverb + "? That's hilarious!");
+
+    }
+
+}
